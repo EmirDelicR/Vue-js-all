56761ac5d2f6ff041b6d69afa1fcc653f8dee743
Emir <emirdelictbf@gmail.com>
2018-11-13 11:52:22 +0100

    Adding BusEvent comunication to event task

--- tasks/Events/src/App.vue
+++ tasks/Events/src/App.vue
@@ -1,17 +1,43 @@
 <template>
   <div id="app">
     <HelloWorld />
+    <app-button button-text="Sparta" @sendDataToPerent="showMe" />
+    <hr />
+    <shopping-input />
+    <shopping-list :shoppingList="shoppingList" />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import CustomButton from './components/Button';
+import ShoppingInput from './components/ShoppingInput';
+import ShoppingList from './components/ShoppingList';
+import EventBus from './EventBus';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    appButton: CustomButton,
+    ShoppingInput,
+    ShoppingList
+  },
+  data () {
+    return {
+      shoppingList: []
+    }
+  },
+  methods: {
+    showMe (childeValue) {
+      console.log(childeValue);
+    }
+  },
+  created () {
+    EventBus.$on('addShoppingItem', (itemName) => {
+      this.shoppingList.push(itemName);
+    })
+  },
 }
 </script>
 
---
+++ tasks/Events/src/components/Button.vue
@@ -0,0 +1,39 @@
+<template>
+    <button @click="clicked">
+        {{buttonText}}
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'CustomButton',
+    data () {
+        return {
+            value: 'This is sparta!'
+        }
+    },
+    props: {
+        buttonText: {
+            type: String,
+            default: () => {
+                return "Fancy Button!"
+            },
+            required: true,
+            validator: value => value.length > 3
+        }
+    },
+    methods: {
+        clicked () {
+            this.$emit('sendDataToPerent', this.value);
+        }
+    },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+button {
+  border: 1px solid black;
+  padding: 10px;
+}
+</style>
