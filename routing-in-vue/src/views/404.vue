ffcc7649ed48b118f29abbafcc8995ed14e8a9b7
Emir Delic Redzepagic <emirdelictbf@gmail.com>
2020-01-10 12:36:27 +0100

    Update 404.vue

--- routing-in-vue/src/views/404.vue
+++ routing-in-vue/src/views/404.vue
@@ -3,7 +3,7 @@
     <h1>Not Found</h1>
     <p>
       Oops we couldn't find that page. Try going
-      <router-link :to="{ name: 'homer'}">Home</router-link>
+      <router-link :to="{ name: 'home'}">Home</router-link>
     </p>
   </div>
 </template>
